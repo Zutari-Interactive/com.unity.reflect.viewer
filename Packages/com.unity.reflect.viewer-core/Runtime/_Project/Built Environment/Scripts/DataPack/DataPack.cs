0945f06f931c3a297ca4c984b02d8ca290daaf1e
murray-walker <murray.walker@zutari.com>
2021-05-18 12:26:58 +0200

    removed weird files in Packages

--- Packages/com.unity.reflect.viewer-core/Runtime/_Project/Built Environment/Scripts/DataPack/DataPack.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "DataPack", menuName = "Zutari Elements/External Data Holder")]
-public class DataPack : ScriptableObject
-{
-    public string[] paths;
-}
--- Packages/com.unity.reflect.viewer-core/Runtime/_Project/Elements/Scripts/Runtime/General/Globals/Enums.cs
+++
@@ -1,8 +0,0 @@
-﻿public enum CharacterMaterial
-{
-    Clothes = 0,
-    Skin = 1,
-    Accessory = 0, // Possible Bug Cause
-}
-
-
